719bddcac5aba2437655659a5d3b04bdad25e4e3
Adam Munns <adam@whetinteractive.com>
2022-11-08 10:00:11 -0800

    rework comonents to prevent reload

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,17 @@ import '../styles/globals.css'
 import { UploaderProvider } from '@w3ui/react-uploader'
 import { AuthProvider } from '@w3ui/react-keyring'
 import { UploadsListProvider } from '@w3ui/react-uploads-list';
-
+import Layout from '../components/layout';
 
 function MyApp({ Component, pageProps }) {
+
   return (
     <AuthProvider>
       <UploaderProvider>
         <UploadsListProvider>
-          <Component {...pageProps} />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
         </UploadsListProvider>
       </UploaderProvider>
     </AuthProvider>
